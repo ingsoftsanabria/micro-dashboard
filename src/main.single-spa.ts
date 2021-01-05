8d25e981c8e5c20444f310414bd8224f31c205f2
ingsoftsanabria <ingsoftsanabria@gmail.com>
2021-01-04 22:17:37 -0500

    config subroutes

--- src/main.single-spa.ts
+++ src/main.single-spa.ts
@@ -21,7 +21,7 @@ const lifecycles = singleSpaAngular({
     singleSpaPropsSubject.next(singleSpaProps);
     return platformBrowserDynamic(getSingleSpaExtraProviders()).bootstrapModule(AppModule);
   },
-  template: '<app-root />',
+  template: '<app-dash-root />',
   Router,
   NavigationStart,
   NgZone,
