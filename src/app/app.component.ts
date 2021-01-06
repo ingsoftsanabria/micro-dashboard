40669b477be727a983dabd04d055d2b8f5de5157
ingsoftsanabria <ingsoftsanabria@gmail.com>
2021-01-05 19:35:05 -0500

    obtener props enviadas desde singleSpaProps

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { singleSpaPropsSubject, SingleSpaProps } from 'src/single-spa/single-spa-props';
 
 @Component({
   selector: 'app-dash-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'micro-dashboard';
+
+  singleSpaProps: SingleSpaProps;
+  subscription: Subscription;
+
+  ngOnInit(): void {
+    this.subscription = singleSpaPropsSubject.subscribe(
+      props => (this.singleSpaProps = props),
+    );
+
+    console.log("authToken", this.singleSpaProps["authToken"]);
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
